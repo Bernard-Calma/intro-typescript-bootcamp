622c5752778b71ec10e8327f992897747e7f693c
Bernard Calma <nard.calma@gmail.com>
2023-05-29 20:20:01 -0700

    chore(folder structure)Create dist and src folders
    Create dist and src folder and moved js and ts files accordingly

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,3 @@
 "use strict";
-// Fucntion Types
-// A Function can be passed into another function
-const fTest = (x, cbf) => {
-    cbf(x);
-};
-const showResult = (x) => {
-    console.log("Result is " + x);
-};
-fTest(1000, showResult);
+let greeting = "Hello World!";
+console.log(greeting);
