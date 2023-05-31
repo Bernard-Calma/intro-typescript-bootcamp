a1a2a985ac7120590c2834978219b652dab2cf71
Bernard Calma <nard.calma@gmail.com>
2023-05-30 19:33:25 -0700

    chore(variables): Variable Declaration
    Declare an object and used enum

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,20 @@
 "use strict";
-let greeting = "Hello World!";
-console.log(greeting);
+var weekDay;
+(function (weekDay) {
+    weekDay["MONDAY"] = "MONDAY";
+    weekDay["TUESDAY"] = "TUESDAY";
+    weekDay["WEDNESDAY"] = "WEDNESDAY";
+    weekDay["THURSDAY"] = "THURSDAY";
+    weekDay["FRIDAY"] = "FRIDAY";
+    weekDay["SATURDAY"] = "SATURDAY";
+    weekDay["SUNDAY"] = "SUNDAY";
+})(weekDay || (weekDay = {}));
+const employee = {
+    empName: "Axle",
+    dependents: 2,
+    comittees: ["Philantrophy", true],
+    payDay: weekDay.FRIDAY
+};
+employee.comittees[1] = false;
+employee.comittees.push("test", true);
+console.log(employee);
--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,45 @@
 
 // fTest(1000, showResult);
 
-let greeting: string = "Hello World!";
-console.log(greeting);
+// let greeting: string = "Hello World!";
+// console.log(greeting);
+
+//
+// An enum is a special "class" that represents a group of constants (unchangeable variables).
+// Enums come in two flavors string and numeric. 
+// enum weekDay {
+//     MONDAY, //1
+//     TUESDAY, //2
+//     WEDNESDAY, //3
+//     THURSDAY, //4
+//     FRIDAY, //5
+//     SATURDAY, //6
+//     SUNDAY //7
+// }
+
+// String Enums
+enum weekDay {
+    MONDAY = "MONDAY",
+    TUESDAY = "TUESDAY",
+    WEDNESDAY = "WEDNESDAY", 
+    THURSDAY = "THURSDAY",
+    FRIDAY = "FRIDAY",
+    SATURDAY = "SATURDAY",
+    SUNDAY = "SUNDAY"
+}
+
+const employee: {
+    empName: string,
+    dependents: number,
+    comittees: [string, boolean]
+    payDay: string
+} = {
+    empName: "Axle",
+    dependents: 2,
+    comittees: ["Philantrophy", true],
+    payDay: weekDay.FRIDAY
+}
+
+employee.comittees[1] = false;
+employee.comittees.push("test", true)
+console.log(employee);
